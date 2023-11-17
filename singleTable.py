33b74dfebb93dac502ccc33dc5b50de245524572
Amandeep Singh (Lab49) <amandeep.rehal@iongroup.com>
2023-11-17 14:55:44 +0530

    Initial commit

---
+++ singleTable.py
@@ -0,0 +1,30 @@
+from sdv.datasets.demo import download_demo
+from sdv.lite import SingleTablePreset
+
+
+real_data, metadata = download_demo(
+    modality='single_table',
+    dataset_name='fake_hotel_guests'
+)
+
+print(real_data.head())
+print(metadata.columns)
+
+synthesizer = SingleTablePreset(
+    metadata,
+    name='FAST_ML'
+)
+
+synthesizer.fit(
+    data=real_data
+)
+
+synthetic_data = synthesizer.sample(
+    num_rows=10
+)
+
+synthesizer.
+
+print(synthetic_data.head())
+
+
