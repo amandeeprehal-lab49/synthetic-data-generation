fa549e7b0ee7dcc62cf482d63a923ebc9fb5d2b5
Amandeep Singh (Lab49) <amandeep.rehal@iongroup.com>
2023-11-21 11:27:59 +0530

    Wrote to json files

--- multiTable.py
+++ multiTable.py
@@ -1,6 +1,7 @@
 from sdv.metadata import MultiTableMetadata
 from sdv.datasets.local import load_csvs
 from sdv.multi_table import HMASynthesizer
+from rdt.transformers.pii import AnonymizedFaker
 
 metadata = MultiTableMetadata()
 metadata.detect_from_csvs(
@@ -8,14 +9,18 @@ metadata.detect_from_csvs(
 )
 metadata.update_column(table_name='contact', column_name='contactId', sdtype='id')
 metadata.set_primary_key(table_name='contact', column_name='contactId');
+metadata.update_column(table_name='contact', column_name='name', sdtype='name')
+metadata.update_column(table_name='contact', column_name='address', sdtype='address')
 
 metadata.update_column(table_name='accountNotes', column_name='id', sdtype='id')
 metadata.set_primary_key(table_name='accountNotes', column_name='id');
 metadata.update_column(table_name='accountNotes', column_name='accountContactId', sdtype='id')
+metadata.update_column(table_name='accountNotes', column_name='notes', sdtype='text')
 
 metadata.update_column(table_name='callTranscript', column_name='id', sdtype='id')
 metadata.set_primary_key(table_name='callTranscript', column_name='id');
 metadata.update_column(table_name='callTranscript', column_name='accountContactId', sdtype='id')
+metadata.update_column(table_name='callTranscript', column_name='transcript', sdtype='text')
 
 metadata.update_column(table_name='accountContact', column_name='id', sdtype='id')
 metadata.set_primary_key(table_name='accountContact', column_name='id');
@@ -24,6 +29,9 @@ metadata.update_column(table_name='accountContact', column_name='contactId', sdt
 
 metadata.update_column(table_name='account', column_name='accountId', sdtype='id')
 metadata.set_primary_key(table_name='account', column_name='accountId');
+metadata.update_column(table_name='account', column_name='notes', sdtype='text')
+metadata.update_column(table_name='account', column_name='address', sdtype='address')
+metadata.update_column(table_name='account', column_name='name', sdtype='company')
 
 metadata.relationships = list()
 
@@ -33,9 +41,6 @@ metadata.add_relationship(parent_table_name='accountContact', parent_primary_key
 
 metadata.add_relationship(parent_table_name='account', parent_primary_key='accountId', child_table_name='accountContact', child_foreign_key='accountId')
 metadata.add_relationship(parent_table_name='contact', parent_primary_key='contactId', child_table_name='accountContact', child_foreign_key='contactId')
-
-
-print(metadata.to_dict)
 
 datasets = load_csvs(
     folder_name='data/client',
@@ -48,8 +53,26 @@ datasets = load_csvs(
 synthesizer = HMASynthesizer(metadata)
 synthesizer.fit(datasets)
 
-synthetic_data = synthesizer.sample(scale=2)
+transformers = synthesizer.get_transformers(table_name='accountNotes')
+transformers['notes'].enforce_uniqueness=True
+transformers = synthesizer.get_transformers(table_name='callTranscript')
+transformers['transcript'].enforce_uniqueness=True
+transformers = synthesizer.get_transformers(table_name='account')
+transformers['notes'].enforce_uniqueness=True
 
-print(synthetic_data['callTranscript'])
-print(synthetic_data['account'])
-print(synthetic_data['contact'])
+addressTransformer = AnonymizedFaker(provider_name='address', function_name='address', enforce_uniqueness=True)
+synthesizer.update_transformers(table_name='account', column_name_to_transformer={'address': addressTransformer})
+companyTransformer = AnonymizedFaker(provider_name='company', function_name='company', enforce_uniqueness=True)
+synthesizer.update_transformers(table_name='account', column_name_to_transformer={'name': companyTransformer})
+
+synthesizer.fit(datasets)
+
+synthetic_data = synthesizer.sample(scale=1)
+
+synthetic_data['account'].to_json('./data/result/account.json', orient='records', lines=True)
+synthetic_data['contact'].to_json('./data/result/contact.json', orient='records', lines=True)
+synthetic_data['accountContact'].to_json('./data/result/accountContact.json', orient='records', lines=True)
+synthetic_data['accountNotes'].to_json('./data/result/accountNotes.json', orient='records', lines=True)
+synthetic_data['callTranscript'].to_json('./data/result/callTranscript.json', orient='records', lines=True)
+
+print('*************** Check result in ./data/result *********************')
